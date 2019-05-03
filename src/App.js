3964c1688b8b623b5343a52a69a160047683ea74
jamespulley59 <jamespulley59@gmail.com>
2019-05-03 10:03:06 -0400

    widget needs finish

--- src/App.js
+++ src/App.js
@@ -7,10 +7,12 @@ import Register from './components/user/Register';
 import WebsiteList from './components/website/WebsiteList';
 import WebsiteNew from './components/website/WebsiteNew';
 import WebsiteEdit from './components/website/WebsiteEdit';
-import PageEdit from './components/page/PageEdit';
 import PageList from './components/page/PageList';
 import PageNew from './components/page/PageNew';
-
+import PageEdit from './components/page/PageEdit';
+import WidgetList from './components/widget/WidgetList';
+import WidgetChooser from './components/widget/WidgetChooser';
+import WidgetEdit from './components/widget/WidgetEdit';
 
 function App() { 
 
@@ -18,15 +20,20 @@ function App() {
     <Router className="app">
       <Switch>
         <Route exact path= "/" component= {Login} />
-        <Route exact path= "/login" component= {Login} />    
-        <Route exact path= "/user/:uid" component= {Profile} />
-        <Route exact path= "/register" component= {Register} />
+        <Route exact path= "/login" component= {Login} />
+        <Route exact path= "/register" component= {Register} />    
+        <Route exact path= "/user/:uid" component= {Profile} />        
         <Route exact path= "/user/:uid/website" component= {WebsiteList} />
         <Route exact path= "/user/:uid/website/new" component= {WebsiteNew} />
         <Route exact path= "/user/:uid/website/:wid" component= {WebsiteEdit} />
-        <Route exact path= "/user/:uid/website/:wid/page/:pid" component= {PageEdit} />
         <Route exact path= "/user/:uid/website/:wid/page" component= {PageList} />
         <Route exact path= "/user/:uid/website/:wid/page/new" component= {PageNew} />
+        <Route exact path= "/user/:uid/website/:wid/page/:pid" component= {PageEdit} />
+        <Route exact path= "/user/:uid/website/:wid/page/:pid/widget	" component= {WidgetList} />
+        <Route exact path= "/user/:uid/website/:wid/page/:pid/widget/new" component= {WidgetChooser} />
+        <Route exact path= "/user/:uid/website/:wid/page/:pid/widget/:wgid	" component= {WidgetEdit} />
+     
+        
       </Switch>
     </Router>
   )  
---
+++ src/components/widgets/WidgetList.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+export default class Login extends Component {
+    render() {
+        return(
+    <nav class="navbar fixed-top navbar-light bg-light">
+        <a href='../page/page-list.html'><i class="fa fa-chevron-left"></i></a>
+        <span class ='navbar-brand mb-0 h1'>Widgets</span>
+        <a href="widget-choose.html"><i class ='fas fa-plus'></i></a>
+    </nav>    
+)
