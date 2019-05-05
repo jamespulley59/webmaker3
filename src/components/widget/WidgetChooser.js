93674e77e09e7c70c8a48562cba7340a2d971958
publaptops <jamespulley59@gmail.com>
2019-05-05 18:21:27 -0400

    widget chooser finished

--- src/components/widget/WidgetChooser.js
+++ src/components/widget/WidgetChooser.js
@@ -4,12 +4,47 @@ export default class WidgetChooser extends Component {
   render() {
     return (
       <div>
-        <nav class="navbar navbar-dark bg-primary fixed-top">
-                <a href='../user/profile.html'>
-                    <i class="fas fa-chevron-left"></i>
-                </a>
-                    <span class="navbar-brand mb-0 h1">Choose Widgets</span><span></span>  
-            </nav>
+        <nav class="navbar navbar-light bg-light fixed-top">
+              <a href='widget-list.html'>
+                  <i class="fas fa-chevron-left"></i>
+              </a>
+                  <span class="navbar-brand mb-0 h1">Choose Widgets</span><span></span>  
+          </nav>
+          <div class='container'>
+            <ul class='list-group list-group-flush'>
+              <li class='list-group-item'>
+                <a href='widget-heading.html'>Header</a>
+              </li>
+              <li class='list-group-item'>
+                <a href='#'>Label</a>
+              </li>
+              <li class='list-group-item'>
+                <a href='#'>HTML</a>                
+              </li>
+              <li class='list-group-item'>
+                <a href='#'>Text Input</a>
+              </li>
+              <li class='list-group-item'>
+                <a href='#'>Link</a>
+              </li>
+              <li class='list-group-item'>
+                <a href='#'>Button</a>                
+              </li>
+              <li class='list-group-item'>
+                <a href='widget-image.html'>Image</a>                
+              </li>
+              <li class='list-group-item'>
+                <a href='widget-youtube.html'>Youtube</a>
+              </li>
+              <li class='list-group-item'>
+                <a href='#'>Data Table</a>
+              </li>
+              <li class='list-group-item'>
+                <a href='#'>Repeater</a>                
+              </li>
+            </ul>
+          </div>
+
       </div>
     )
   }
