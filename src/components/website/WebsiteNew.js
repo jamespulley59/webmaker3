0532b2cf12deb6859083f85f206c4436e26d414b
jamespulley59 <jamespulley59@gmail.com>
2019-05-10 11:19:05 -0400

    websiteNew finished, uid issue

--- src/components/website/WebsiteNew.js
+++ src/components/website/WebsiteNew.js
@@ -1,84 +1,145 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import uuid from "uuid";
+
 export default class WebsiteNew extends Component {
+
+state = {
+    uid: this.props.match.params.uid,
+    websites: [],
+    name: "",
+    description: ""
+};
+
+componentDidMount() {
+    this.filterWebsites(this.props.websites);
+}
+
+filterWebsites = websites => {
+    const newWebsites = websites.filter(
+        website => website.developerId === this.state.uid
+    );
+    this.setState({
+        websites: newWebsites
+    });
+};
+
+onChange = e => {
+    this.setState({
+        [e.target.name]: e.target.value
+    });
+};
+
+onSubmit = e => {
+    const { name, description, uid } = this.state;
+    e.preventDefault();
+    const newWeb = {
+        _id: uuid(),
+        name,
+        developerId: uid,
+        description
+    };
+    this.props.addWeb(newWeb);
+    this.props.history.push(`/user/${this.state.uid}/website`);
+};
+
+
   render() {
     return (
 <div>
-<nav className="navbar navbar-dark bg-primary fixed-top row nav-height">
-    <div className='col-lg-4 d-none d-sm-block'>
-        <Link to='/user/:uid/website' className='color-white'>
-            <i className="fas fa-chevron-left"></i>
-        </Link>            
-        <Link className='color-white padding-left' to='/user/:uid/website'>
-            <h5 className='display-inline'>Websites</h5>
-        </Link>
-            <a className='color-white float-right' to='#'>
-                <i class='fas fa-plus'></i>
-            </a>
-    </div>
-    <div className='col-sm-8'>                     
-        <Link className="color-white d-sm-none" to='/user/:uid/website'>        
-        </Link>            
-        <Link className='float-right pt-2' to='/user/:uid/website'>
-            <i className="fas fa-chevron-left"></i>
-        </Link>
-           
-    </div>             
-</nav>
-<section className='row'>
-    <div className='col-lg-4 d-none d-lg-block'>    
-    <div className='container'>
-    <ul className='list-group'>
-        <li className='list-group-item'>
-            <Link to='/user/:uid/website/:wid/pag'>Address Book App</Link>
-            <Link className='float-right' to='/user/:uid/website/:wid'>
-                <i className="fas fa-cog"></i>
+    <nav className="navbar navbar-light bg-primary fixed-top row nav-height">
+        <div className='col-lg-4 d-none d-lg-block text-center text-white'>
+            <Link to='/user/:uid/website' className='float-left'>
+                <i className="fas fa-chevron-left"></i>
             </Link>
-        </li>        
-        <li className='list-group-item'>
-            <Link to='/user/:uid/website/:wid/page'>Blogger</Link>
-            <Link className='float-right' to='/user/:uid/website/:wid'>
-                <i className="fas fa-cog"></i>
+                <span className=''>Websites</span>            
+            <Link className='float-right'  to='/user/:uid/website/:wid/page/newl'>
+                 <i class='fas fa-plus-square'></i>
+            </Link>             
+        </div>                 
+        
+        <div className='col-lg-8 text-center text-white'>
+            <Link
+                className='d-lg-none float-left'
+                to='/user/:uid/website'>            
+                    <i className='fas fa-arrow-circle-left'></i>
             </Link>
-        </li>
-        <li className='list-group-item'>
-            <Link to='/user/:uid/website/:wid/page'>Blogging App</Link>
-            <Link className='float-right' to='/user/:uid/website/:wid'>
-                <i className="fas fa-cog"></i>
+                <span className=''>New Website</span>
+            <Link className='float-right' to='/user/:uid/website'>
+                <i className='far fa-check-circle'></i>
             </Link>
-        </li>
-        <li className='list-group-item'>
-            <Link to='/user/:uid/website/:wid/page'>Script Test App</Link>
-            <Link className='float-right' to='/user/:uid/website/:wid'>
-                <i className="fas fa-cog"></i>
-            </Link>
-        </li>  
-    </ul> 
-    </div>            
-    </div>
-    <div className='col-lg-8'>
-        <div className='container'>
-            <form>
-                <div className='form-group'>
-                    <label htmlFor='name'>Name</label>
-                        <input placeholder='Enter Website Name Here...' className='form-control' type="text" id='name' name='name' />
+        </div>          
+    </nav>
+
+    <div className='row'>
+        <div className='col-4 d-none d-sm-block'>
+            <div className='container-fluid'>
+                <ul className='list-group'>
+                    {this.state.websites.map(website => (
+                        <li key={website._id} className='list-group-item'>
+                            <Link to={`/user/${uid}/website/${website._id}/page`}>
+                            {website.name}                          </Link>    
+                            <Link className='float-right' 
+                                to={`/user/${uid}/website/${website._id}`}>        
+                                   <i className='fas fa-cog' />    
+                            </Link>                   
+                        </li>                      
+                    ))} 
+                </ul>
+            </div>    
+        </div> 
+
+        <div className='col-sm-8'>
+            <div className='container-fluid'>
+                <form id='newWebForm' onSubmit={this.onSubmit}>
+                    <div className='form-group'>
+                        <label htmlFor='name'>
+                            <b>Name</b>
+                        </label>
+                        <input id='name'
+                               name='name'                          className='form-control'
+                               type='text'
+                               placeholder='Name of new Website'
+                               onChange={this.onChange}
+                               value={this.state.name} /> 
+                    </div>                                
+                    
+                        <div className='form-group'>
+                            <label htmlFor='description'>
+                                <b>Description</b>
+                            </label>
+                            <textarea id='description'
+                                      rows='5'
+                                      className='form-control'
+                                      type='text'
+                                      placeholder='Describe the new Website'
+                                      name='description'
+                                      onChange={this.onChange}
+                                      value={this.state.description} />  
+                        </div>                            
+                        
+                        <Link to={`/user/${uid}/website`}
+                            className='btn btn-lg btn-warning'
+                        >Cancel
+                        </Link>                         
+                        <button className='btn btn-lg btn-success float-right'>
+                            Submit
+                        </button>
+                    </form>
                 </div>
-                <div className='form-group'>
-                    <label htmlFor='description'>Description</label>
-                    <textarea rows='5' placeholder='Enter Website Description Here...' className='form-control' id='description' name='description'></textarea>
-                </div>
-            </form>
+            </div>
         </div>
-    </div>
-</section>
-    <nav className="navbar navbar-dark bg-primary fixed-bottom">
-        <div className='full-width'>             
-                <Link to='../user/profile.html'>
-                    <i className=' float-right fas fa-user'></i>
+        <nav className="navbar navbar-dark bg-primary fixed-bottom">
+            <div className='full-width'>             
+                <Link className='color-white float-right'to={`/user/${uid}`}>
+                    <i className='fas fa-user'></i>
                 </Link>            
-        </div>
-    </nav> 
+            </div>
+        </nav>                 
 </div>
     )
   }
 }
+        
+                      
+    
